68b3593479291e3612a7eaf23bfebd11ace90961
Kunal <kunalsolanke1000@gmail.com>
2020-08-26 23:48:12 +0530

    e

--- accounts/managers.py
+++ accounts/managers.py
@@ -6,14 +6,14 @@ from django.contrib.auth.base_user import BaseUserManager
 
 class UserManager(BaseUserManager) :
 
-    def create_user(self,username,password):
-        user = self.model(username= username)
+    def create_user(self,username,email,password):
+        user = self.model(username= username,email=email)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self,username,password):
-        user = self.create_user(username,password)
+    def create_superuser(self,username,email,password):
+        user = self.create_user(username,email,password)
 
         user.is_superuser=True
         user.is_admin =True
